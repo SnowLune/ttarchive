71fcb2c7e42434dd0b33ccdc1405cdcaf44c770b
Luna R <snowluna@protonmail.com>
2022-08-18 01:04:51 -0400

    css tweaks, js pause

--- share/user.js
+++ share/user.js
@@ -1,14 +1,22 @@
+function mobileScroll() {
+
+}
+
 var videoMain = document.querySelector(".video-main");
+var playingVideos = [];
 
 videoMain.addEventListener("click", (event) => {
    console.log(event.target);
 
    if (event.target.paused) {
-      event.target.scrollIntoView({ behavior: "smooth", block: "center" });
+      event.target.scrollIntoView({ behavior: "smooth", block: "end" });
+      playingVideos.forEach((video) => video.pause());
+      playingVideos = [];
       event.target.play();
-      document.addEventListener("scroll", () => {
-         console.log(window.pageYOffset - event.target.offsetTop);
-      })
+      playingVideos.push(event.target);
+      // document.addEventListener("scroll", () => {
+      //    console.log(window.pageYOffset - event.target.offsetTop);
+      // })
    } else event.target.pause();
 });
 
@@ -17,3 +25,22 @@ videoMain.addEventListener("dblclick", (event) => {
    event.target.play();
    event.target.controls = true;
 });
+
+var scrollVal
+document.addEventListener("scroll", (event) => {
+   if (scrollVal) {
+      if (window.scrollY < scrollVal) {
+         console.log("up")
+         console.log(scrollVal)
+         scrollVal = window.scrollY;
+      }
+      else if (window.scrollY > scrollVal) {
+         console.log("down")
+         console.log(scrollVal)
+         scrollVal = window.scrollY;
+      }
+   }
+   else {
+      scrollVal = window.scrollY;
+   }
+})
